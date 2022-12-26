370d4099a97bde30bac0722028299a96776961b5
ice949 <justineimasiku21@gmail.com>
2022-12-26 13:23:47 +0200

    Add labels to input fields

--- src/main.py
+++ src/main.py
@@ -2,11 +2,20 @@ import kivy
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
+from kivy.properties import ObjectProperty
 
 # kivy.require('1.9.0')
-    
+   
         
 class Nicetyfarm(App):
+    
+    name = ObjectProperty(None)
+    phone = ObjectProperty(None)
+    
+    def btn (self):
+        name = self.name.text
+        phone = self.phone.text
+        # print("Name:" self.name, "Phone:" self.phone)
 
     def build(self):
         return FloatLayout()
