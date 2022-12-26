c8243816bb4c001425b57fad5486dcf4c463084e
ice949 <justineimasiku21@gmail.com>
2022-12-26 12:31:14 +0200

    start building the signin page layout

--- src/main.py
+++ src/main.py
@@ -1,29 +1,15 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
 
 # kivy.require('1.9.0')
-
-class Mygrid(Widget):
-    name = ObjectProperty(None)
-    email = ObjectProperty(None)
-    
-    
-    def btn(self):
-        print("Name:", self.name.text, "Email:", self.email.text)
-        self.name.text = ""
-        self.email.text = ""
     
         
 class Nicetyfarm(App):
 
     def build(self):
-        return Mygrid()
+        return FloatLayout()
 
 
 if __name__ == "__main__":
